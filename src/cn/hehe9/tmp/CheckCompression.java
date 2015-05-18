0d9146a3c071f96e8ffd14dc2a0d4182a9aef45f
96688huang <96688huang@163.com>
2015-05-19 01:00:35 +0800

    TOMCAT 启用页面压缩功能;

---
+++ src/cn/hehe9/tmp/CheckCompression.java
@@ -0,0 +1,24 @@
+package cn.hehe9.tmp;
+
+import org.apache.http.client.HttpClient;
+
+public class CheckCompression {
+//	public static void main(String[] args) throws Exception {
+//		HttpClient http = new HttpClient();
+//		GetMethod get = new GetMethod("http://localhost:8080/login.htm");
+//		try {
+//			get.addRequestHeader("accept-encoding", "gzip,deflate");
+//			get.addRequestHeader("user-agent",
+//					"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0; Alexa Toolbar; Maxthon 2.0)");
+//			int er = http.executeMethod(get);
+//			if (er == 200) {
+//				System.out.println(get.getResponseContentLength());
+//				String html = get.getResponseBodyAsString();
+//				System.out.println(html);
+//				System.out.println(html.getBytes().length);
+//			}
+//		} finally {
+//			get.releaseConnection();
+//		}
+//	}
+}
